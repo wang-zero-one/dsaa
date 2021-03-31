57ccb8b21729c713567184fba4748085d635df62
wanggaofei <972617883@qq.com>
2021-03-31 17:00:29 +0800

    数组队列

--- dateStructures/src/main/java/com/bluesky/queue/ArrayQueue.java
+++ dateStructures/src/main/java/com/bluesky/queue/ArrayQueue.java
@@ -1,6 +1,55 @@
 package com.bluesky.queue;
 
+import java.util.Scanner;
+
 public class ArrayQueue {
+    public static void main(String[] args) {
+        //创建一个队列
+        Queue queue = new Queue(3);
+        char key = ' ' ;
+        Scanner scanner = new Scanner(System.in);
+        boolean loop  = true;
+        //输出一个菜单
+        while (loop){
+            System.out.println("s(show)：显示队列");
+            System.out.println("e(exit)：退出程序");
+            System.out.println("a(add)：添加数据到队列");
+            System.out.println("g(get)：从队列取出数据");
+            System.out.println("h(head)：查看队列头的数据");
+            //接收一个字符
+            key = scanner.next().charAt(0);
+            switch (key){
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g':
+                    try{
+                       int res =  queue.getQueue();
+                        System.out.printf("取出的数据是%d\n",res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                case 'h':
+                    try{
+                        int res =  queue.headQueue();
+                        System.out.printf("队列头的数据是%d\n",res);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                case 'e':
+                   scanner.close();
+                   loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
 }
 
